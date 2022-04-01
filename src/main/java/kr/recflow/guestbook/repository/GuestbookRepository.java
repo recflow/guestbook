8f6dcf6d2b06010857b38683fdba05f3a03f0993
recflow <chjh9107@gmail.com>
2022-04-01 20:57:38 +0900

    add QuerydslPredicateExecutor - p.141

--- src/main/java/kr/recflow/guestbook/repository/GuestbookRepository.java
+++ src/main/java/kr/recflow/guestbook/repository/GuestbookRepository.java
@@ -2,6 +2,7 @@ package kr.recflow.guestbook.repository;
 
 import kr.recflow.guestbook.entity.Guestbook;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
-public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long>, QuerydslPredicateExecutor<Guestbook> {
 }
