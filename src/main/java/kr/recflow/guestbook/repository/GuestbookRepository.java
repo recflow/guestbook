b37ffaa621e9908ba7b22dd90124315203684731
recflow <chjh9107@gmail.com>
2022-04-01 20:16:39 +0900

    Create GuestbookRepository extends JpaRepository  - p.137

---
+++ src/main/java/kr/recflow/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,7 @@
+package kr.recflow.guestbook.repository;
+
+import kr.recflow.guestbook.entity.Guestbook;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface GuestbookRepository extends JpaRepository<Guestbook, Long> {
+}
