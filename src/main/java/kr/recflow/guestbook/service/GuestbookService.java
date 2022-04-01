a3a02bfe1b927997f47dbbbb3b2e5e60cd4d34d9
recflow <chjh9107@gmail.com>
2022-04-01 21:57:34 +0900

    DTO to Entity - p.154

--- src/main/java/kr/recflow/guestbook/service/GuestbookService.java
+++ src/main/java/kr/recflow/guestbook/service/GuestbookService.java
@@ -1,7 +1,17 @@
 package kr.recflow.guestbook.service;
 
 import kr.recflow.guestbook.dto.GuestbookDTO;
+import kr.recflow.guestbook.entity.Guestbook;
 
 public interface GuestbookService {
     Long register(GuestbookDTO dto);
+
+    default Guestbook dtoToEntity(GuestbookDTO dto) {
+        return Guestbook.builder()
+                .gno(dto.getGno())
+                .title(dto.getTitle())
+                .content(dto.getContent())
+                .writer(dto.getWriter())
+                .build();
+    }
 }
