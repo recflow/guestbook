6dc76ecbe7abab02aac39f589770101800a42ed5
recflow <chjh9107@gmail.com>
2022-04-01 22:09:12 +0900

    Use DTO to Entity - p.155

--- src/main/java/kr/recflow/guestbook/service/GuestbookServiceImpl.java
+++ src/main/java/kr/recflow/guestbook/service/GuestbookServiceImpl.java
@@ -1,6 +1,7 @@
 package kr.recflow.guestbook.service;
 
 import kr.recflow.guestbook.dto.GuestbookDTO;
+import kr.recflow.guestbook.entity.Guestbook;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
@@ -10,6 +11,10 @@ public class GuestbookServiceImpl implements GuestbookService{
 
     @Override
     public Long register(GuestbookDTO dto) {
+        log.info("DTO---------------------------");
+        log.info(dto);
+        Guestbook entity = dtoToEntity(dto);
+        log.info(entity);
         return null;
     }
 }
